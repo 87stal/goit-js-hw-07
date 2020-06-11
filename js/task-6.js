1d8e93b6d40ff30ef1db1bb871d732f9c87d9b0b
87stal <87stal@gmail.com>
2020-06-11 19:45:05 +0300

    Correct task-7

--- js/task-6.js
+++ js/task-6.js
@@ -1,12 +1,13 @@
 const inputRef = document.querySelector('#validation-input');
 const inputLengthRef = inputRef.getAttribute('data-length');
 
-inputRef.addEventListener('focus', handleInputBlur);
+inputRef.addEventListener('blur', handleInputBlur);
 
 function handleInputBlur(event) {
+
   if (event.target.value.length === Number(inputLengthRef)) {
-    inputRef.id = 'validation-input.valid';
+    inputRef.id = 'validation-input-valid';
   } else {
-    inputRef.id = 'validation-input.invalid';
+    inputRef.id = 'validation-input-invalid';
   }
 }
